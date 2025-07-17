cdd2d4eb961465f01cb497ebf8c6850362477433
Shivam <shivamprajapati@gmail.com>
2025-07-18 01:04:27 +0530

    new

--- practices/main.tf
+++ practices/main.tf
@@ -7,6 +7,11 @@ resource "azurerm_resource_group" "rg011" {
     location = "westus"
 }
 
+resource "azurerm_resource_group" "rg011" {
+    name = "final-rg011"
+    location = "westus"
+}
+
 
 
 resource "azurerm_storage_account" "sa011" {
